6a8a327e6b455697d289f2c04ad0d63b90119e64
whymeeverytime <borgahaktanbilen01@gmail.com>
2020-11-30 21:29:12 +0300

    Removed ASCII

--- MindBendersComplete/src/MindBendersComplete.java
+++ MindBendersComplete/src/MindBendersComplete.java
@@ -23,13 +23,6 @@ public class MindBendersComplete {
         System.out.println( "     _|  _|       _|    _|     _|_|_|   _|      _|     _|_|_|   _|_|_|_|       _|         _|_|_|   _|           _|_|_|       _|_|   _|   _|    _|    _|     _|_|_|  " );
         System.out.println( "                                   _|                                                                               _|                                              " );
         System.out.println( "                               _|_|                                                                             _|_|                                                " );
-        /*Secondary ASCII art
-        System.out.println( "██     ██ ██   ██ ██    ██ ███    ███ ███████ ███████ ██    ██ ███████ ██████  ██    ██ ████████ ██ ███    ███ ███████" );
-        System.out.println( "██     ██ ██   ██  ██  ██  ████  ████ ██      ██      ██    ██ ██      ██   ██  ██  ██     ██    ██ ████  ████ ██      " );
-        System.out.println( "██  █  ██ ███████   ████   ██ ████ ██ █████   █████   ██    ██ █████   ██████    ████      ██    ██ ██ ████ ██ █████   " );
-        System.out.println( "██ ███ ██ ██   ██    ██    ██  ██  ██ ██      ██       ██  ██  ██      ██   ██    ██       ██    ██ ██  ██  ██ ██      " );
-        System.out.println( " ███ ███  ██   ██    ██    ██      ██ ███████ ███████   ████   ███████ ██   ██    ██       ██    ██ ██      ██ ███████ " );
-        */
         
         // Welcoming messages
         System.out.println();
